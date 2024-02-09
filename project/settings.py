45b02962000659e5093dfdaae7a99f4143e94a12
maksfow <maksfow@gmail.com>
2024-02-09 15:57:04 +0500

    register

--- project/settings.py
+++ project/settings.py
@@ -75,42 +75,42 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-   # 'default': {
-       # 'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-   # }
-    'users': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'save_users',
-        'USER': 'postgres',
-        'PORT': 5432,
-        'PASSWORD': 'niginaiuldasheva200',
-        'HOST': 'localhost'
-    },
-    'articles': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'save_articles',
-        'USER': 'postgres',
-        'PORT': 5432,
-        'PASSWORD': 'niginaiuldasheva200',
-        'HOST': 'localhost'
-    },
-    'date':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'save_date',
-        'USER': 'postgres',
-        'PORT': 5432,
-        'PASSWORD': 'niginaiuldasheva200',
-        'HOST': 'localhost'
-    },
-    'editing':{
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'editing',
-        'USER': 'postgres',
-        'PORT': 5432,
-        'PASSWORD': 'niginaiuldasheva200',
-        'HOST': 'localhost'
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'users': {
+     #   'ENGINE': 'django.db.backends.postgresql',
+     #   'NAME': 'save_users',
+      #  'USER': 'postgres',
+      #  'PORT': 5432,
+      #  'PASSWORD': 'niginaiuldasheva200',
+      #  'HOST': 'localhost'
+   # },
+   # 'articles': {
+      #  'ENGINE': 'django.db.backends.postgresql',
+      #  'NAME': 'save_articles',
+      #  'USER': 'postgres',
+      #  'PORT': 5432,
+      #  'PASSWORD': 'niginaiuldasheva200',
+      #  'HOST': 'localhost'
+    #},
+    #'date':{
+       # 'ENGINE': 'django.db.backends.postgresql',
+       # 'NAME': 'save_date',
+       # 'USER': 'postgres',
+       # 'PORT': 5432,
+       # 'PASSWORD': 'niginaiuldasheva200',
+       # 'HOST': 'localhost'
+   # },
+    #'editing':{
+       # 'ENGINE': 'django.db.backends.postgresql',
+       # 'NAME': 'editing',
+       # 'USER': 'postgres',
+       # 'PORT': 5432,
+       # 'PASSWORD': 'niginaiuldasheva200',
+       # 'HOST': 'localhost'
+    #},
 }
 
 
